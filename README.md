cfd7f289178c1920a9c403bfb2882c24991c2bed
tinyvelvetine <164116351+tinyvelvetine@users.noreply.github.com>
2024-03-20 03:09:52 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# weareweb3-gallery-suite
+
+Explore a gallery of projects and applications, showcasing the potential of decentralized technologies.
+
+## Installation
+
+```bash
+git clone https://github.com/tinyvelvetine/weareweb3-gallery-suite.git
+cd weareweb3-gallery-suite
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+const weareweb3_suite = require('weareweb3-suite');
+const weareweb3_gallery_suite = require('weareweb3-gallery-suite');
+const supertest = require('supertest');
+const commander = require('commander');
+const eth_crypto = require('eth-crypto');
+const node_sass = require('node-sass');
+const passport = require('passport');
+const cors = require('cors');
+const moment = require('moment');
+const dotenv = require('dotenv');
+const react = require('react');
+const ejs = require('ejs');
+const bluebird = require('bluebird');
+const lodash = require('lodash');
+const async = require('async');
+const webpack_cli = require('webpack-cli');
+const xml2js = require('xml2js');
+const react_dom = require('react-dom');
+const nodemon = require('nodemon');
+const request = require('request');
+const web3_utils = require('web3-utils');
+
+const greet = (name) => {
+  console.log(`Greetings, ${name}!`);
+};
+greet('Alice');
+
+const http2 = require('http2');
+const server = http2.createSecureServer();
+server.on('stream', (stream, headers) => {
+  stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+  stream.end('<h1>Hello HTTP/2</h1>');
+});
+server.listen(8443);
+
+const fs = require('fs');
+const stream = fs.createReadStream('file.txt', 'utf8');
+stream.on('data', chunk => {
+  console.log('Chunk received:', chunk);
+});
+
+console.log(`Current date and time: ${new Date().toLocaleString()}`);
